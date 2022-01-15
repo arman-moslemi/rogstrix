eadcfa1decb288478a459524a255cf21f9f152d6
yasii30.yts <yasii30.yts@gmail.com>
2022-01-15 15:49:24 +0330

    HomePage

--- src/components/features/Home/HomePageComponents/WhyRogstrix.js
+++ src/components/features/Home/HomePageComponents/WhyRogstrix.js
@@ -1,17 +1,47 @@
 import react from "react";
 import "./Styles/whyrogstrix.css"
-import MouseImg from "../../assets/images/MouseImg.png"
+import MouseImg from "../../assets/images/MouseImg.png";
+import Calender from "../../../assets/icons/calender";
+import Garantee from "../../../assets/icons/garantee";
+import Location from "../../../assets/icons/location";
 import { Container ,Col, Button,Row} from "react-bootstrap";
 const Header = () => {
   return (
    
       <div className="whyRogstrix">
-       <Container className="d-flex-center" fluid>
+       <Container className="row pad-right-6 pad-left-6" fluid>
         
-            <Col md={7}>
-
+            <Col md={7} id="secondCol">
+            <h3 className="boxTitle mar-bottom-45 mar-top-45">چرا راگ استریکس ؟</h3>
+            <div className="d-flex-start-start mar-bottom-30">
+              <div className="iconBox">
+               <Location/>
+              </div>
+              <div>
+                <p className="firstLine">ارسال به سراسر کشور</p>
+                <p className="secondLine">امکان سفارش کالا از سراسر کشور،ارسال سریع و آسان کالا به سراسر کشور</p>
+                </div>
+            </div>
+            <div className="d-flex-start-start mar-bottom-30">
+              <div className="iconBox">
+               <Garantee/>
+              </div>
+              <div>
+                <p className="firstLine">ضمانت اصالت کالا</p>
+                <p className="secondLine">خرید کالای اصل ایسوس راگ با بیمه نامه معتبر.</p>
+                </div>
+            </div>
+            <div className="d-flex-start-start mar-bottom-30">
+              <div className="iconBox">
+               <Calender/>
+              </div>
+              <div>
+                <p className="firstLine">ضمانت بازگشت ۷ روزه</p>
+                <p className="secondLine">امکان بازگشت محصول در صورت مشکل فنی یا مغایرت کالا</p>
+                </div>
+            </div>
             </Col>
-            <Col md={5}>
+            <Col md={5} id="firstCol">
                <img src={MouseImg} className="mouseImg"/>
             </Col>
         
--- src/components/features/Home/layouts/Footer.js
+++ src/components/features/Home/layouts/Footer.js
@@ -1,13 +1,139 @@
-
+import react from "react";
+import "../../../assets/css/styles.css";
+import RedFooterLogo from "../../../assets/icons/redFooterLogo";
+import instafooter from "../../../assets/img/insta-footer.png";
+import locationFooter from "../../../assets/img/location-footer.png";
+import mailFooter from "../../../assets/img/mail-footer.png";
+import phoneFooter from "../../../assets/img/phone-footer.png";
+import telegram from "../../../assets/img/telegram.png";
+import twitter from "../../../assets/img/twitter.png";
+import linkdin from "../../../assets/img/linkdin.png";
+import insta from "../../../assets/img/insta.png";
+import facebook from "../../../assets/img/facebook.png";
+import aparat from "../../../assets/img/aparat.png";
+import up from "../../../assets/img/up.png";
+import { Container ,Col, Button,Row} from "react-bootstrap";
 
 
 const Footer = () => {
   return (
-    <div className="">
-      <div className="container">
+   <div>
+      <div className="backgroundFooter">
+      <Container className="footerBg" fluid>
+        <div className="row">
+          <Col md={4}>
+            <p className="footerTitle">
+            راگ استریکس
+            </p>
+            <div className="d-flex-start-start alignItem-center mar-bottom-2">
+              <div className="iconBoxFooter">
+              <img src={phoneFooter}/>
+              </div>
+              <div>
+                <a href="#" className="footerItem">021-7523698</a>
+              </div>
+            </div>
+            <div className="d-flex-start-start alignItem-center mar-bottom-2">
+              <div className="iconBoxFooter">
+              <img src={mailFooter}/>
+              </div>
+              <div>
+                <a href="#" className="footerItem">giftcard@gmail.com</a>
+              </div>
+            </div>
+            <div className="d-flex-start-start alignItem-center mar-bottom-2">
+              <div className="iconBoxFooter">
+              <img src={instafooter}/>
+              </div>
+              <div>
+                <a href="#" className="footerItem">@giftcardsocialmedia</a>
+              </div>
+            </div>
+            <div className="d-flex-start-start alignItem-center mar-bottom-2">
+              <div className="iconBoxFooter">
+              <img src={locationFooter}/>
+              </div>
+              <div style={{width:250}}>
+                <a href="#" className="footerItem">تهران، خیابان آزادی، کوچه فلان، ساختمان فلان، پلاک فلان
+</a>
+              </div>
+            </div>
+          </Col>
+          <Col md={4}>
+          <p className="footerTitle">
+            لینک های مهم
+            </p>
+            <ul className="footerUL">
+              <li>
+                <a href="">
+                  راهنمای خرید
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  قوانین و مقررات
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  وبلاگ
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  درباره ما
+                </a>
+              </li>
+            </ul>
+          </Col>
+          <Col md={4} className="ta-center">
+          <RedFooterLogo/>
+          <br/>
+          <p className="mailTitle">عضویت در خبرنامه</p>
+          <div className="d-flex-start-start">
+            <div>
+              <input placeholder="نشانی ایمیل خود را وارد کنید"  className="emailInput"/>
+            </div>
+            <div>
+              <button type="submit" className="submitBtn">عضویت</button>
+            </div>
+          </div>
+          <div className="socialRow">
+            <div className="socialIcon">
+              <img src={aparat}/>
+            </div>
+            <div className="socialIcon">
+              <img src={linkdin}/>
+            </div>
+            <div className="socialIcon">
+              <img src={facebook} style={{height:27,width:13}}/>
+            </div>
+            <div className="socialIcon">
+              <img src={telegram}/>
+            </div>
+            <div className="socialIcon">
+              <img src={twitter}/>
+            </div>
+            <div className="socialIcon">
+              <img src={insta}/>
+            </div>
+          </div>
+          </Col>
+        </div>
+        <div className="row">
+          <Col md={12} className="ta-center">
+            <button className="upBtn">
+            <img src={up}/>
+            </button>
+          </Col>
+        </div>
         
-      </div>
+      </Container>
     </div>
+    <div className="lastFooterRow">
+      <p>کلیه حقوق مادی و معنوی برای «گروه فلان»، سایت فلان، محفوظ است.</p>
+    </div>
+   </div>
   );
 };
 export default Footer;
