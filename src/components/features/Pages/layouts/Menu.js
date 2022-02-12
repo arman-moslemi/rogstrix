c90518151b8d2036a682a1f91eb84ebe1e93642f
yasii30.yts <yasii30.yts@gmail.com>
2022-02-12 15:22:58 +0330

    menu

--- src/components/features/Pages/layouts/Menu.js
+++ src/components/features/Pages/layouts/Menu.js
@@ -1,4 +1,4 @@
-import react from "react";
+import {react,useState} from "react";
 import RedLogo from '../../../assets/img/redLogo.png';
 import { FaChevronDown } from 'react-icons/fa';
 import { Container ,Col, Button,Dropdown,DropdownButton} from "react-bootstrap";
@@ -8,25 +8,21 @@ import MenuCase from "../../../assets/icons/menuCase";
 import MouseMenu from "../../../assets/icons/mouseMenu";
 import RedMenuLogo from "../../../assets/icons/redMenuIcon";
 import UpdateMenu from "../../../assets/icons/updateMenu";
-import ReactMegaMenu from "react-mega-menu";
-const Test1 = () => {
-   return <div className="">
+import MegaMenuCamera from "../../../assets/icons/MegaMenuCamera";
+import MegaMenuCase from "../../../assets/icons/MegaMenuCase";
+import MegaMenuConsole from "../../../assets/icons/MegaMenuConsole";
+import MegaMenuLapTop from '../../../assets/icons/MegaMenuLapTop';
+import MegaMenuMobile from "../../../assets/icons/MegaMenuMobile";
+import MegaMenuPrinter from "../../../assets/icons/MegaMenuPrinter";
+import MegaMenuServer from "../../../assets/icons/MegaMenuServer";
+import MegaMenuTablet from "../../../assets/icons/MegaMenuTablet";
 
-<div className="row" style={{marginRight:"0px",marginLeft:"0px"}}>
- <Col md={12}>
-    <p>salam</p>
- </Col>
-</div>
-   </div>
 
- }
-const dataMegaMenu = [
-   {
-     label: 'لامپ', key: 1, items: <Test1 />
-   }
- 
- ]
 const Menu = () => {
+   const [showMega, setShowMega] = useState(false);
+const onClick = () =>{
+   setShowMega(!showMega);
+};
   return (
    <Container fluid className="pad0">
    <div className="mainBack">
@@ -51,24 +47,438 @@ const Menu = () => {
         <UpdateMenu className="marginLeft15"/>
         ارتقائ لپ تاپ
      </Button>
-     <Dropdown>
-  <Dropdown.Toggle variant="success" id="dropdown-basic" className="MainMenuBtn borderRight1">
+     <Button className="MainMenuBtn borderRight1" onClick={onClick}>
   <MouseMenu className="marginLeft15"/>
   مشاهده محصولات
-  </Dropdown.Toggle>
+  <FaChevronDown/>
+  </Button>
 
-  <Dropdown.Menu>
-    <ReactMegaMenu
-    data={dataMegaMenu}
-    />
-  </Dropdown.Menu>
-</Dropdown>
      <Button className="MainMenuBtn borderLeft1">
       <RedMenuLogo/>
      </Button>
       
      </div>
    </div>
+   {showMega ?<div className="megaMenu">
+      <div className="MegaMenuFirstRow">
+         <a className="megaMenuFirstRowBox" href="#">
+            <MegaMenuCase/>
+         </a>
+         <a className="megaMenuFirstRowBox" href="#">
+            <MegaMenuLapTop/>
+         </a>
+         <a className="megaMenuFirstRowBox" href="#">
+            <MegaMenuTablet/>
+         </a>
+         <a className="megaMenuFirstRowBox" href="#">
+            <MegaMenuMobile/>
+         </a>
+         <a className="megaMenuFirstRowBox" href="#">
+            <MegaMenuServer/>
+         </a>
+         <a className="megaMenuFirstRowBox" href="#">
+            <MegaMenuPrinter/>
+         </a>
+         <a className="megaMenuFirstRowBox" href="#">
+            <MegaMenuCamera/>
+         </a>
+         <a className="megaMenuFirstRowBox" href="#">
+            <MegaMenuConsole/>
+         </a>
+      </div>
+      <div className="MegaMenuContainer">
+         <div className="MegaMenuCol">
+            <ul>
+               <li className="listTitle">
+                  <a href="#">
+                     مانیتور
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مانیتور ایسوس
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مانیتور ال جی
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مانیتور سامسونگ
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مانیتور ویوسونیک
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مانیتور بنکیو
+                  </a>
+               </li>
+             
+            </ul>
+            <ul>
+               <li className="listTitle">
+                  <a href="#">
+                     سی پی یو
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     اینتل
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                    آی ام دی
+                  </a>
+               </li>
+           
+            
+             
+            </ul>
+            <ul>
+               <li className="listTitle">
+                  <a href="#">
+                     مادربورد
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مادربورد ایسوس
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مادربورد ام اس آی
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مادربورد گیگابایت
+                  </a>
+               </li>
+        
+            </ul>
+         </div>
+         <div className="MegaMenuCol">
+            <ul>
+               <li className="listTitle">
+                  <a href="#">
+                     مانیتور
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مانیتور ایسوس
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مانیتور ال جی
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مانیتور سامسونگ
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مانیتور ویوسونیک
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مانیتور بنکیو
+                  </a>
+               </li>
+             
+            </ul>
+            <ul>
+               <li className="listTitle">
+                  <a href="#">
+                     سی پی یو
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     اینتل
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                    آی ام دی
+                  </a>
+               </li>
+           
+            
+             
+            </ul>
+            <ul>
+               <li className="listTitle">
+                  <a href="#">
+                     مادربورد
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مادربورد ایسوس
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مادربورد ام اس آی
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مادربورد گیگابایت
+                  </a>
+               </li>
+        
+            </ul>
+         </div>
+         <div className="MegaMenuCol">
+            <ul>
+               <li className="listTitle">
+                  <a href="#">
+                     مانیتور
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مانیتور ایسوس
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مانیتور ال جی
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مانیتور سامسونگ
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مانیتور ویوسونیک
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مانیتور بنکیو
+                  </a>
+               </li>
+             
+            </ul>
+            <ul>
+               <li className="listTitle">
+                  <a href="#">
+                     سی پی یو
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     اینتل
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                    آی ام دی
+                  </a>
+               </li>
+           
+            
+             
+            </ul>
+            <ul>
+               <li className="listTitle">
+                  <a href="#">
+                     مادربورد
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مادربورد ایسوس
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مادربورد ام اس آی
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مادربورد گیگابایت
+                  </a>
+               </li>
+        
+            </ul>
+         </div>
+         <div className="MegaMenuCol">
+            <ul>
+               <li className="listTitle">
+                  <a href="#">
+                     مانیتور
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مانیتور ایسوس
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مانیتور ال جی
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مانیتور سامسونگ
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مانیتور ویوسونیک
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مانیتور بنکیو
+                  </a>
+               </li>
+             
+            </ul>
+            <ul>
+               <li className="listTitle">
+                  <a href="#">
+                     سی پی یو
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     اینتل
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                    آی ام دی
+                  </a>
+               </li>
+           
+            
+             
+            </ul>
+            <ul>
+               <li className="listTitle">
+                  <a href="#">
+                     مادربورد
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مادربورد ایسوس
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مادربورد ام اس آی
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مادربورد گیگابایت
+                  </a>
+               </li>
+        
+            </ul>
+         </div>
+         <div className="MegaMenuCol">
+            <ul>
+               <li className="listTitle">
+                  <a href="#">
+                     مانیتور
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مانیتور ایسوس
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مانیتور ال جی
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مانیتور سامسونگ
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مانیتور ویوسونیک
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مانیتور بنکیو
+                  </a>
+               </li>
+             
+            </ul>
+            <ul>
+               <li className="listTitle">
+                  <a href="#">
+                     سی پی یو
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     اینتل
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                    آی ام دی
+                  </a>
+               </li>
+           
+            
+             
+            </ul>
+            <ul>
+               <li className="listTitle">
+                  <a href="#">
+                     مادربورد
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مادربورد ایسوس
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مادربورد ام اس آی
+                  </a>
+               </li>
+               <li className="listItems">
+                  <a href="#">
+                     مادربورد گیگابایت
+                  </a>
+               </li>
+        
+            </ul>
+         </div>
+      </div>
+   </div>:null}
    </Container>
      
    
