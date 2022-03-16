e40d60954516a8802618bfd35c00447a1c0e7d77
yasii30.yts <yasii30.yts@gmail.com>
2022-03-16 11:51:21 +0330

    factor

--- src/components/features/Pages/Factor.js
+++ src/components/features/Pages/Factor.js
@@ -12,7 +12,7 @@ import Checkbox from '@mui/material/Checkbox';
 import {useParams } from "react-router-dom";
 import React,{useState,useEffect} from 'react'
 import { apiUrl ,apiAsset} from "../../../commons/inFormTypes";
-
+import LogoBlack from "../../assets/img/Logo-black.png";
 export const truncate = (str, len) => {
     // console.log("truncate", str, str.length, len);
     if (str?.length > len && str?.length > 0) {
@@ -79,10 +79,267 @@ const Factor = () => {
   
 
  
-    <Container className="UserPanelContainer" fluid style={{backgroundColor:'#fff'}}>
-  <div className="row">
-    <Col md={12}>
-      <div className="factorTable">
+    <Container className="" fluid style={{backgroundColor:'#f4f4f4',padding:0}}>
+      <div className="row factorHeader">
+        <Col md={12}>
+         <img src={LogoBlack} className="factorLogo"/>
+        </Col>
+      </div>
+      <Container>
+        <p className="fontWeightBold mb-2">
+          پیش فاکتور
+        </p>
+        <p className="fontWeightBold mb-4">
+          مشخصات سفارش
+        </p>
+      <div className="row pdTable">
+    <Col md={12} className="pd0">
+    <div className="row">
+          <Col md={4} className="tableBorderLeft d-flex pdTable2 borderBottomTable">
+            <p className="fontWeightBold">شماره فاکتور : 
+            </p>
+            <p className="fontWeightNormal ml-2">5023687</p>
+          </Col>
+          <Col md={4} className="tableBorderLeft pdTable2 d-flex borderBottomTable">
+          <p className="fontWeightBold">تاریخ :  
+            </p>
+            <p className="fontWeightNormal ml-2">29/12/00</p>
+          </Col>
+          <Col md={4} className="tableBorder pdTable2 d-flex borderBottomTable">
+          <p className="fontWeightBold">نام و نام خانوادگی :  
+            </p>
+            <p className="fontWeightNormal ml-2">حامد مصلحی</p>
+          </Col>
+        </div>
+        <div className="row">
+          <Col md={4} className="tableBorderLeft d-flex pdTable2 borderBottomTable">
+            <p className="fontWeightBold">کد ملی : 
+            </p>
+            <p className="fontWeightNormal ml-2">00162525252</p>
+          </Col>
+          <Col md={4} className="tableBorderLeft pdTable2 d-flex borderBottomTable">
+          <p className="fontWeightBold">شماره تماس : 
+            </p>
+            <p className="fontWeightNormal ml-2">09122525252</p>
+          </Col>
+          <Col md={4} className="tableBorder pdTable2 d-flex borderBottomTable">
+          <p className="fontWeightBold">ایمیل : 
+            </p>
+            <p className="fontWeightNormal ml-2">info@gmail.com</p>
+          </Col>
+        </div>
+        <div className="row">
+          <Col md={4} className="tableBorderLeft d-flex pdTable2 borderBottomTable">
+            <p className="fontWeightBold">کد پستی :  
+            </p>
+            <p className="fontWeightNormal ml-2">1212113131</p>
+          </Col>
+          <Col md={4} className="tableBorderLeft pdTable2 d-flex borderBottomTable">
+          <p className="fontWeightBold">شماره بارنامه :   
+            </p>
+            <p className="fontWeightNormal ml-2">----</p>
+          </Col>
+          <Col md={4} className="tableBorder pdTable2 d-flex borderBottomTable">
+          <p className="fontWeightBold">وضعیت پرداخت :   
+            </p>
+            <p className="fontWeightNormal ml-2">پرداخت نشده</p>
+          </Col>
+        </div>
+        <div className="row">
+          <Col md={12} className=" d-flex pdTable2 borderBottomTable">
+            <p className="fontWeightBold">آدرس : 
+            </p>
+            <p className="fontWeightNormal ml-2">استان تهران،شهر تهران،پاسداران،فلان جا</p>
+          </Col>
+        
+        </div>
+     
+    </Col>
+  </div>
+  <div className="pdTable">
+  <div className="topBarTable">
+    <div className="row">
+      <Col md={6} className="ta-right">
+      <p className="fontWeightBold2 mb-2">
+          شرح محصول
+        </p>
+      </Col>
+      
+      <Col md={2} className="ta-right">
+      <p className="fontWeightBold2 mb-2">
+         قیمت واحد
+        </p>
+      </Col>
+      <Col md={1} className="ta-right">
+      <p className="fontWeightBold2 mb-2">
+          تخفیف
+        </p>
+      </Col>
+      <Col md={1} className="ta-right">
+      <p className="fontWeightBold2 mb-2">
+         تعداد
+        </p>
+      </Col>
+      <Col md={2} className="ta-right">
+      <p className="fontWeightBold2 mb-2">
+        قیمت کل
+        </p>
+      </Col>
+    </div>
+   
+  </div>
+  <div className="row pd15 borderBottomTable">
+      <Col md={6} className="ta-right">
+      <p className="fontWeightNormal2 mb-2">
+          مانیتور سامسونگ
+          گارانتی طلایی
+        </p>
+      </Col>
+      
+      <Col md={2} className="ta-right">
+      <p className="fontWeightNormal2 mb-2">
+         1.500.000 تومان
+        </p>
+      </Col>
+      <Col md={1} className="ta-right">
+      <p className="fontWeightNormal2 mb-2">
+          0
+        </p>
+      </Col>
+      <Col md={1} className="ta-right">
+      <p className="fontWeightNormal2 mb-2">
+         1
+        </p>
+      </Col>
+      <Col md={2} className="ta-right">
+      <p className="fontWeightNormal2 mb-2">
+        1.500.000 تومان
+        </p>
+      </Col>
+    </div>
+    <div className="row pd15 borderBottomTable">
+      <Col md={6} className="ta-right">
+      <p className="fontWeightNormal2 mb-2">
+          مانیتور سامسونگ
+          گارانتی طلایی
+        </p>
+      </Col>
+      
+      <Col md={2} className="ta-right">
+      <p className="fontWeightNormal2 mb-2">
+         1.500.000 تومان
+        </p>
+      </Col>
+      <Col md={1} className="ta-right">
+      <p className="fontWeightNormal2 mb-2">
+          0
+        </p>
+      </Col>
+      <Col md={1} className="ta-right">
+      <p className="fontWeightNormal2 mb-2">
+         1
+        </p>
+      </Col>
+      <Col md={2} className="ta-right">
+      <p className="fontWeightNormal2 mb-2">
+        1.500.000 تومان
+        </p>
+      </Col>
+    </div>
+    <div className="row pd15 backBlueLight">
+      <Col md={6} className="ta-right">
+      <p className="fontWeightNormal2 mb-2">
+          هزینه ارسال از طریق پست سفارشی  :
+          زمان فوق با توجه به محدوده جغرافیایی به صورت تقریبی بین 2 تا 4 روز کاری متفاوت است.تحویل در بازه زمانی 10-17.سقف بیمه پست برای هر محصول 20 میلیون تومان است.
+        </p>
+      </Col>
+      
+      <Col md={2} className="ta-right">
+      <p className="fontWeightNormal2 mb-2">
+        12.500 تومان
+        </p>
+      </Col>
+      <Col md={1} className="ta-right">
+      <p className="fontWeightNormal2 mb-2">
+          -
+        </p>
+      </Col>
+      <Col md={1} className="ta-right">
+      <p className="fontWeightNormal2 mb-2">
+        -
+        </p>
+      </Col>
+      <Col md={2} className="ta-right">
+      <p className="fontWeightNormal2 mb-2">
+        12.500 تومان
+        </p>
+      </Col>
+    </div>
+    <div className="row pd15 borderBottomTable">
+      <Col md={6} className="ta-right">
+      <p className="fontWeightNormal2 mb-2">
+          مجمع کل : 
+        </p>
+      </Col>
+      
+      <Col md={2} className="ta-right">
+      <p className="fontWeightNormal2 mb-2">
+        -
+        </p>
+      </Col>
+      <Col md={1} className="ta-right">
+      <p className="fontWeightNormal2 mb-2">
+          0
+        </p>
+      </Col>
+      <Col md={1} className="ta-right">
+      <p className="fontWeightNormal2 mb-2">
+         1
+        </p>
+      </Col>
+      <Col md={2} className="ta-right">
+      <p className="fontWeightNormal2 mb-2">
+        1.512.000 تومان
+        </p>
+      </Col>
+    </div>
+   
+    <div className="row backGray">
+      <Col md={8} className="ta-right">
+     
+      </Col>
+      <Col md={2} className="ta-right">
+      <p className="fontWeightBold2 mb-2">
+         مبلغ قابل پرداخت : 
+        </p>
+      </Col>
+      <Col md={2} className="ta-right">
+      <p className="fontWeightBold2 mb-2">
+        1.512.200 تومان
+        </p>
+      </Col>
+    </div>
+
+ 
+  </div>
+  <div className="pdTable pdM mt-2">
+   <p className="fontWeightBold">
+         توضیحات خریدار : 
+        </p>
+        <br/>
+        <p className="fontWeightNormal2">
+        کنسل شد
+        </p>
+   </div>
+   <p className="fontWeightBold mt-4">
+         خریدار محترم : 
+        </p>
+        <br/>
+        <p className="fontWeightNormal2">
+        کلیه قوانین و مقررات مربوط به خرید اینترنتی از وب سایت ما،مندرج و قابل مشاهده است.لذا خرید کالا از این شرکت به منزله پذیرش کلیه قوانین و مقررات اعم از قانون تجارت الکترونیک و قانون حمایت از حقوق مصرف کننده و دستورالعمل صادره از اتحادیه کسب و کار مجازی وزارت صنعت و معدن و تجارت بوده و در محاکم قابل استفاده است.
+        </p>
+  {/* <div className="factorTable">
+        
         <div className="row rightMenuBox1" style={{backgroundColor:'#111'}}>
           <Col md={4} className="ta-right">
           <p className="fontWeightBold colorWhite" href="#">
@@ -157,9 +414,8 @@ const Factor = () => {
           })
         }
        
-      </div>
-    </Col>
-  </div>
+      </div> */}
+      </Container>
     
     </Container>
    
