5538cc20553df9279fef64adddae3223d1f62052
amir333 <a.mofid76@yahoo.com>
2022-10-18 11:20:53 +0330

    ss

--- src/components/features/Pages/SingleProductComponents/CommentBox.js
+++ src/components/features/Pages/SingleProductComponents/CommentBox.js
@@ -110,7 +110,7 @@ setOpen(true)
               data?.map((item)=>{
 return(
 
-<div className="mt-3">
+<div className="mt-3" style={{border:'1px dotted #f6303f',padding:25,borderRadius:5}}>
     <div className="d-flex align-items-center">
         <div>
         <img className="profileComment" src={UserProfile}/>
@@ -148,9 +148,24 @@ index+1>item.Rate?
 
 
 </div>
+
 )
               })
             }
+           <div style={{display:'flex',marginTop:20}}>
+<img src={UserProfile} style={{width:50,height:50,marginLeft:10}}/>
+<div style={{display:'flex',flexDirection:'column'}}>
+<p style={{fontFamily:'IRANSans-Medium',marginBottom:5}}>
+  ادمین
+</p>
+<p style={{fontFamily:'IRANSans-Light',color:'#898989',fontSize:12}}>
+  ۰۱/۰۹/۱۲
+</p>
+</div>
+           <div style={{backgroundColor:'#fff',height:120,width:'90%',border:'1px dotted #d7d7d7',borderRadius:5,marginRight:'auto'}}>
+
+</div>
+           </div>
 <hr className="dottedH" id="borderRed"/>
             {/* <div className="mt-3">
                 <div className="d-flex align-items-center">
